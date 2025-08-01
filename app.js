fe474cf4b983333bcf33d83fdab239ac8cb4ff93
Takunda Mundawarwo <tkmundawarwo@gmail.com>
2025-08-01 12:13:21 +0200

    Add data.js with task list

--- app.js
+++ app.js
@@ -1,24 +1,4 @@
-const tasks = [
-  {
-    id: 1,
-    title: "Launch Epic Career",
-    description: "Create a killer Resume",
-    status: "todo",
-  },
-  {
-    id: 2,
-    title: "Master JavaScript",
-    description: "Get comfortable with the fundamentals",
-    status: "doing",
-  },
-  {
-    id: 3,
-    title: "Contribute to Open Source Projects",
-    description:
-      "Gain practical experience and collaborate with others in the software development community",
-    status: "done",
-  },
-];
+import { tasks } from "./data.js";
 
 while (tasks.length < 6) {
   addTask();
