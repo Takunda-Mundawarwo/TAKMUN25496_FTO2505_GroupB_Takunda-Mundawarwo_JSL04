1bdc1e26c14753219cb91ed779c9deafa49d00e4
Takunda Mundawarwo <tkmundawarwo@gmail.com>
2025-08-01 17:15:26 +0200

    Add function openModal
    Add event listener to open modal when a task is clicked

--- app.js
+++ app.js
@@ -3,9 +3,27 @@ import { tasks } from "./data.js";
 const toDoList = document.getElementById("todo-list");
 const doingList = document.getElementById("doing-list");
 const doneList = document.getElementById("done-list");
+const tasksSection = document.getElementById("tasks-section");
+
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("close-btn");
+const modalTitle = document.getElementById("modal-title");
+const modalDescription = document.getElementById("modal-description");
+const modalStatus = document.getElementById("modal-status");
 
 //render all tasks on app load
 tasks.forEach(renderTask);
+
+// Event delegation to open modal
+tasksSection.addEventListener("click", function (event) {
+  if (event.target.id === "task") {
+    //Find selected task & open modal with task details
+    const selectedTask = tasks.find(
+      (task) => task.id == event.target.dataset.id
+    );
+    openModal(selectedTask);
+  }
+});
 
 /**
  * @typedef {Object} Task
@@ -20,7 +38,7 @@ tasks.forEach(renderTask);
  */
 function renderTask(task) {
   const renderedTask = document.createElement("div");
-  renderedTask.classList.add("task");
+  renderedTask.setAttribute("id", "task");
 
   renderedTask.innerText = task.title;
   renderedTask.setAttribute("data-id", task.id);
@@ -38,4 +56,20 @@ function renderTask(task) {
       doneList.appendChild(renderedTask);
       break;
   }
+}
+
+/**
+ * Opens the modal either populated with information for
+ * a selected task, or with no task infomation.
+ *
+ * @param {Task} task - The Task Object (optional)
+ */
+function openModal(task = { title: "", description: "", status: "todo" }) {
+  //populate modal
+  modalTitle.value = task.title;
+  modalDescription.value = task.description;
+  modalStatus.selectedIndex = ["todo", "doing", "done"].indexOf(task.status);
+
+  //show modal
+  modal.classList.remove("hidden");
 }
